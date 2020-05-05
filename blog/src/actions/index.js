e66c5f5ca27e254db521fb79527d249f7543ef1e
Pedro Leung <pedro_leung@hotmail.com>
2020-05-05 17:12:16 +1000

    redux store design

---
+++ blog/src/actions/index.js
@@ -0,0 +1,37 @@
+import _ from 'lodash';
+import jsonPlaceholder from '../apis/jsonPlaceholder';
+
+// Solution 2 - Better solution
+export const fetchPostsAndUsers = () => async (dispatch, getState) => {
+  await dispatch(fetchPosts());
+
+  // const userIds = _.uniq(_.map(getState().posts, 'userId'));
+  // userIds.forEach(id => dispatch(fetchUser(id)));
+
+  _.chain(getState().posts)
+    .map('userId')
+    .uniq()
+    .forEach(id => dispatch(fetchUser(id)))
+    .value(); // value acts as execute all actions
+};
+
+export const fetchPosts = () => async dispatch => {
+  const response = await jsonPlaceholder.get('/posts');
+
+  dispatch({ type: 'FETCH_POSTS', payload: response.data });
+};
+
+export const fetchUser = (id) => async dispatch => {
+  const response = await jsonPlaceholder.get(`/users/${id}`);
+
+  dispatch({ type: 'FETCH_USER', payload: response.data });
+};
+
+// Solution 1 - Problem: Data user updated
+// export const fetchUser = (id) => dispatch => _fetchUser(id, dispatch);
+
+// const _fetchUser = _.memoize(async (id, dispatch) => {
+//   const response = await jsonPlaceholder.get(`/users/${id}`);
+
+//   dispatch({ type: 'FETCH_USER', payload: response.data });
+// });
